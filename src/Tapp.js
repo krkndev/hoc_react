3bf847325e2fdd60daac39133bde04e634de3407
MorfinWorkin <morfinworking@gmail.com>
2022-10-21 17:11:48 -0500

    Add withExternalData call to MyTable component

--- src/Tapp.js
+++ src/Tapp.js
@@ -4,16 +4,18 @@ import ImgOverlayExample from './components/DataFetching/Card2';
 import withDataFetch from './components/DataFetching/DataFetchHOC';
 import CardExample from './components/DataFetching/Card';
 import { Row } from 'react-bootstrap';
+import MyTable from './components/TableHOC/MyTable';
+import withExternalData from './components/TableHOC/TableHoc';
+import TestComponent from './components/TestComponent';
 
 const Tapp = () => {
   const FirstDummy = withDataFetch(CardExample);
   const SecondDummy = withDataFetch(ImgOverlayExample);
+  const External = withExternalData(TestComponent);
+  const External2 = withExternalData(MyTable);
   return (
     <>
-      <Row>
-        {/* <FirstDummy /> */}
-        <SecondDummy />
-      </Row>
+      <External2 />
     </>
   );
 };
