f11b2a822d7898cf7a21d6e3f0154860e217b3f5
MorfinWorkin <morfinworking@gmail.com>
2022-10-24 17:11:44 -0500

    Remove unused components

--- src/Tapp.js
+++ src/Tapp.js
@@ -1,21 +1,14 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
-import ImgOverlayExample from './components/DataFetching/Card2';
-import withDataFetch from './components/DataFetching/DataFetchHOC';
-import CardExample from './components/DataFetching/Card';
-import { Row } from 'react-bootstrap';
 import MyTable from './components/TableHOC/MyTable';
 import withExternalData from './components/TableHOC/TableHoc';
-import TestComponent from './components/TestComponent';
 
 const Tapp = () => {
-  const FirstDummy = withDataFetch(CardExample);
-  const SecondDummy = withDataFetch(ImgOverlayExample);
-  const External = withExternalData(TestComponent);
   const External2 = withExternalData(MyTable);
   return (
     <>
-      <External2 />
+      {/* <MyTable /> */}
+      <External2 filterCat closeOnSelect />
     </>
   );
 };
