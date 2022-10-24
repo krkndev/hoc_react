67d26e22df1a218304e596d8f07a08e863d8b8ad
MorfinWorkin <morfinworking@gmail.com>
2022-10-24 17:14:45 -0500

    Wrapp sortable body funtion inside a try catch.

--- src/components/TableHOC/utilities/filters.js
+++ src/components/TableHOC/utilities/filters.js
@@ -55,49 +55,53 @@ export const searchInTable = () => {
 };
 
 export const sortTable = (n) => {
-  let x,
-    i,
-    y,
-    shouldSwitch,
-    switchcount = 0;
-  let table = document.getElementById('myTable');
-  let switching = true;
-  let dir = 'asc';
+  try {
+    let x,
+      i,
+      y,
+      shouldSwitch,
+      switchcount = 0;
+    let table = document.getElementById('myTable');
+    let switching = true;
+    let dir = 'asc';
 
-  while (switching) {
-    switching = false;
-    let rows = table.rows;
-    for (i = 1; i < rows.length - 1; i++) {
-      shouldSwitch = false;
-      x = rows[i].getElementsByTagName('TD')[n];
-      y = rows[i + 1].getElementsByTagName('TD')[n];
-      if (dir === 'asc') {
-        if (
-          x.innerHTML.toString().toLowerCase() >
-          y.innerHTML.toString().toLowerCase()
-        ) {
-          shouldSwitch = true;
-          break;
-        }
-      } else if (dir === 'desc') {
-        if (
-          x.innerHTML.toString().toLowerCase() <
-          y.innerHTML.toString().toLowerCase()
-        ) {
-          shouldSwitch = true;
-          break;
+    while (switching) {
+      switching = false;
+      let rows = table.rows;
+      for (i = 1; i < rows.length - 1; i++) {
+        shouldSwitch = false;
+        x = rows[i].getElementsByTagName('TD')[n];
+        y = rows[i + 1].getElementsByTagName('TD')[n];
+        if (dir === 'asc') {
+          if (
+            x.innerHTML.toString().toLowerCase() >
+            y.innerHTML.toString().toLowerCase()
+          ) {
+            shouldSwitch = true;
+            break;
+          }
+        } else if (dir === 'desc') {
+          if (
+            x.innerHTML.toString().toLowerCase() <
+            y.innerHTML.toString().toLowerCase()
+          ) {
+            shouldSwitch = true;
+            break;
+          }
         }
       }
-    }
-    if (shouldSwitch) {
-      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
-      switching = true;
-      switchcount++;
-    } else {
-      if (switchcount === 0 && dir === 'asc') {
-        dir = 'desc';
+      if (shouldSwitch) {
+        rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
         switching = true;
+        switchcount++;
+      } else {
+        if (switchcount === 0 && dir === 'asc') {
+          dir = 'desc';
+          switching = true;
+        }
       }
     }
+  } catch (error) {
+    console.log('error :>> ', error);
   }
 };
