d4e6ba04ac44b167d1602ee2eba7f442d7fab986
Juan A. Porras Brambila <rraspo@Motherboard.localdomain>
2022-10-26 17:46:55 -0500

    Add new config object in table hoc. WIP MyTable new config.

--- src/Tapp.js
+++ src/Tapp.js
@@ -4,11 +4,43 @@ import MyTable from './components/TableHOC/MyTable';
 import withExternalData from './components/TableHOC/TableHoc';
 
 const Tapp = () => {
-  const External2 = withExternalData(MyTable);
+  const External2 = withExternalData(MyTable, {
+    "request": {
+      "url": "https://dummyjson.com/products",
+      "method": "GET"
+    },
+    "columns": [
+      { id: "id" },
+      { id: "title" },
+      { id: "description" },
+      { id: "price" },
+      { id: "discountPercentage" },
+      { id: "rating" },
+      { id: "stock" },
+      { id: "brand" },
+      { id: "category" },
+      { id: "thumbnail" },
+      { id: "images" }
+    ],
+    "paging": false,
+    "ordering": false,
+  });
+  // const External = withExternalData(MyTable, {
+  //   "request": {
+  //     "url": "https://dummyjson.com/users",
+  //     "method": "GET"
+  //   },
+  //   "columns": [
+  //     {
+
+  //     }
+  //   ]
+  // });
   return (
     <>
       {/* <MyTable /> */}
       <External2 filterCat closeOnSelect />
+      {/* <External filterCat closeOnSelect /> */}
     </>
   );
 };
--- src/components/TableHOC/TableHoc.jsx
+++ src/components/TableHOC/TableHoc.jsx
@@ -1,80 +1,97 @@
 import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 import { Row } from 'react-bootstrap';
 
 const withExternalData =
-  (Component) =>
-  ({ ...props }) => {
-    const [url, setUrl] = useState('');
-    const [data, setData] = useState();
-    const [header, setHeader] = useState();
-    const [loading, setLoading] = useState(false);
-    if (loading) return <div> Loading...</div>;
-    const generateHeaders = (keys) => {
-      const ignoreFilters = ['description', 'discountpercentage', 'thumbnail'];
-      const header = keys.map((key) => ({
-        value: key,
-        label: key.toUpperCase(),
-        filter: !ignoreFilters.includes(key.toLowerCase()) ? true : false,
-        slug: key.toLowerCase(),
-      }));
-      return header;
-    };
+  (Component, config) =>
+    ({ ...props }) => {
+      console.log('config :>> ', config);
 
-    const removeProp = (obj, prop) => {
-      for (let i = 0; i < obj.length; i++) {
-        delete obj[i][prop];
-      }
-      return obj;
-    };
+      const [url, setUrl] = useState(config.request.url);
+      const [data, setData] = useState();
+      const [header, setHeader] = useState();
+      const [loading, setLoading] = useState(false);
 
-    const getData = (url) => {
-      const searchFor = url.substring(url.lastIndexOf('/') + 1);
-      new Promise((resolve, reject) => {
-        setLoading(true);
-        resolve(
+      if (loading) return <div> Loading...</div>;
+
+      // const generateHeaders = () => {
+      //   console.log('config.columns :>> ', config.columns);
+      //   const header = config.columns.map((key) => ({
+      //     value: key,
+      //     label: key.toUpperCase(),
+      //     filter: !ignoreFilters.includes(key.toLowerCase()) ? true : false,
+      //     slug: key.toLowerCase(),
+      //   }));
+      //   return header;
+      // };
+
+      const removeProp = (obj, prop) => {
+        for (let i = 0; i < obj.length; i++) {
+          delete obj[i][prop];
+        }
+        return obj;
+      };
+
+      const getData = (url) => {
+        new Promise((resolve, reject) => {
+          setLoading(true);
           fetch(url, {
-            method: 'GET',
-            cache: 'default',
+            method: config.request.method
           })
             .then((res) => res.json())
             .then((res) => {
-              const keys = Object.keys(res[searchFor][0]);
-              keys.pop();
-              setHeader(generateHeaders(keys));
-              const tmp = removeProp(res[searchFor], 'images');
-              setData(tmp);
-              setLoading(false);
+              setHeader(config.columns);
+              // // const tmp = removeProp(res[searchFor], 'images');
+              // setData(res.products);
+              // setLoading(false);
+              // resolve(res);
             })
-            .catch((err) => window.alert(err)),
-        );
-        reject((error) => {
-          window.alert(error.status);
+            .catch((err) => {
+              setLoading(false);
+              window.alert(err);
+              reject(err);
+            });
         });
-      });
+      };
+
+      return (
+        <>
+          <Row style={{ width: '30%', margin: 'auto', paddingTop: '5%' }}>
+            <input
+              id='pokeInput'
+              type='text'
+              onChange={(e) => {
+                setUrl(e.target.value);
+              }}
+            ></input>
+            <button
+              id='fetch_test'
+              onClick={() => {
+                getData(url);
+              }}
+            >
+              Submit
+            </button>
+          </Row>
+          <Component body={data && data} header={header && header} {...props} />
+        </>
+      );
     };
 
-    return (
-      <>
-        <Row style={{ width: '30%', margin: 'auto', paddingTop: '5%' }}>
-          <input
-            id='pokeInput'
-            type='text'
-            onChange={(e) => {
-              setUrl(e.target.value);
-            }}
-          ></input>
-          <button
-            id='fetch_test'
-            onClick={() => {
-              getData(url);
-            }}
-          >
-            Submit
-          </button>
-        </Row>
-        <Component body={data && data} header={header && header} {...props} />
-      </>
-    );
-  };
+
+withExternalData.propTypes = {
+  /** Receives a table where the data will be filled. */
+  component: PropTypes.element,
+  /** Receives the request config. */
+  request: PropTypes.shape({
+    url: PropTypes.string.isRequired,
+    method: PropTypes.string
+  }),
+  /** Receives the request config. */
+  // headers: PropTypes.shape({
+  //   url: PropTypes.string.isRequired,
+  //   method: PropTypes.string
+  // }),
+};
 
 export default withExternalData;
