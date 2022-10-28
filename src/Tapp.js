1cf11e498d5974e28988b15b14ccfc05ae9625ab
MorfinWorkin <morfinworking@gmail.com>
2022-10-28 12:44:02 -0500

    Add tsc withExternalData import

--- src/Tapp.js
+++ src/Tapp.js
@@ -1,30 +1,31 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import MyTable from './components/TableHOC/MyTable';
-import withExternalData from './components/TableHOC/TableHoc';
+// import withExternalData from './components/TableHOC/TableHoc';
+import withExternalData from './components/TableHOC/TscTest.tsx';
 
 const Tapp = () => {
   const External2 = withExternalData(MyTable, {
-    "request": {
-      "url": "https://dummyjson.com/products",
-      "method": "GET",
-      "target": "products"
+    request: {
+      url: 'https://dummyjson.com/products',
+      method: 'GET',
+      target: 'products',
     },
-    "columns": [
-      { id: "id" },
-      { id: "title", sortable: true },
-      { id: "description" },
-      { id: "price" },
-      { id: "discountPercentage" },
-      { id: "rating" },
-      { id: "stock" },
-      { id: "brand" },
-      { id: "category" },
-      { id: "thumbnail" },
-      { id: "images" }
+    columns: [
+      { id: 'id' },
+      { id: 'title', sortable: true },
+      { id: 'description' },
+      { id: 'price' },
+      { id: 'discountPercentage' },
+      { id: 'rating' },
+      { id: 'stock' },
+      { id: 'brand' },
+      { id: 'category' },
+      { id: 'thumbnail' },
+      { id: 'images' },
     ],
-    "paging": false,
-    "ordering": false,
+    paging: false,
+    ordering: false,
   });
   // const External = withExternalData(MyTable, {
   //   "request": {
